d9e6550fdd33aec34ed708eb7032ed4431a14139
Sashok <sergeevbass@gmail.com>
2022-10-18 20:19:17 +0300

    Complete to resolve second tas

--- SecondHomeWorkOnC#/Program.cs
+++ SecondHomeWorkOnC#/Program.cs
@@ -14,43 +14,31 @@
 // Console.WriteLine($"И вот она: {SecDigit(result)}.");
 
 
-// ----------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа
 // или сообщает, что третьей цифры нет.
 
-int thirdDigit(int num) 
-{
-    int n = 999;
-    if (num < 100)
-        return -1;
-    if (num > n)
-    while(num > n) {
-            num /= 10;
-            if(num < 1000)
-            num %= 10;
-            return num;
-        }
-    else 
-    num %= 10; 
-    return num;
-
-
-}
-Console.WriteLine("Ведите пожалуйста число и увидите его третью цифру: ");
-int result = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Третья цифра такая: {thirdDigit(result)}");
-
-// 645 -> 5
-
-// 78 -> третьей цифры нет
-
-// 32679 -> 6
+// int thirdDigit(int num) 
+// {
+//     if (num < 100)
+//         return 0;
+//     while(num > 999) {
+//             num /= 10;
+//             if(num < 1000)
+//             break;
+//         }
+//     num %= 10; 
+//     return num;
+// }
+// Console.WriteLine("Ведите пожалуйста число и увидите его третью цифру: ");
+// int result = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Третья цифра такая: {thirdDigit(result)}");
 
 
 
-// ----------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
 
 
@@ -70,6 +58,3 @@ Console.WriteLine($"Третья цифра такая: {thirdDigit(result)}");
 // int day = Convert.ToInt32(Console.ReadLine());
 // string answer = DayOfWeek(day);
 // Console.WriteLine(answer);
-
-
-
